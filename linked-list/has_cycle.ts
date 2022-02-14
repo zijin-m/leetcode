c5b422837c17acc73baa2430431ad9360b47bce8
Ethan <meshethan@gmail.com>
2022-02-14 15:45:48 +0800

    ✨ feat(link): 环型链表

--- linked-list/has_cycle.ts
+++ linked-list/has_cycle.ts
@@ -1,14 +1,10 @@
 /**
+链接：https://leetcode-cn.com/leetbook/read/linked-list/jbex5/
 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
 
 如果链表中存在环 ，则返回 true 。 否则，返回 false 。
 
-作者：力扣 (LeetCode)
-链接：https://leetcode-cn.com/leetbook/read/linked-list/jbex5/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
  */
-
 /**
  * Definition for singly-linked list.
  */
